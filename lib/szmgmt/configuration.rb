cfb6f5bdaafb51c560ca5770b957520dd1d84c15
Tomas Simacek <simacektomas@volny.cz>
2018-04-06 23:00:09 +0200

    Configuration class for application.

---
+++ lib/szmgmt/configuration.rb
@@ -0,0 +1,14 @@
+module SZMGMT
+  class Configuration < Struct.new(:root_dir, :schema_dir)
+    DEFAULT_OPTIONS = {
+        root_dir: '/etc/szmgmt/',
+        schema_dir: '/etc/szmgmt/schema',
+    }
+
+    def initialize(options = {})
+      DEFAULT_OPTIONS.merge(options).each do |option, value|
+        self[option] = value
+      end
+    end
+  end
+end
