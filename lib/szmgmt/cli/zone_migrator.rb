16f663a17012d5aff7d474ec69b1f3327db9ac78
Tomas Simacek <simacektomas@volny.cz>
2018-04-29 17:58:30 +0200

    Finished migrator rework

--- lib/szmgmt/cli/zone_migrator.rb
+++ lib/szmgmt/cli/zone_migrator.rb
@@ -28,7 +28,7 @@ module SZMGMT
         puts "  ---------------------------------------------------------"
         puts "  Connecting concurrently to hosts '#{@zones_by_hosts.keys.join(' ')}' to perform migration (direct)."
         result_all = Parallel.map(@zones_by_hosts.keys, in_threads: @zones_by_hosts.keys.size) do |host_name|
-          Parallel.map(@zones_by_hosts[host_name], in_processes: @zones_by_hosts[host_name].size) do |zone|
+          Parallel.map(@zones_by_hosts[host_name], in_threads: @zones_by_hosts[host_name].size) do |zone|
             log_name = "#{zone}_migration_#{rand(36**6).to_s(36)}.log"
             routine_options[:logger] = Logger.new(File.join(log_dir, log_name))
             puts "  Processing migration of zone '#{zone}:#{host_name}' to host #{@dest_host_spec[:host_name]}. See log '#{File.join(log_dir, log_name)}'."
@@ -67,13 +67,27 @@ module SZMGMT
         puts "    Rewrite existing zones: #{force ? "enable" : "disable"}"
         puts "  ---------------------------------------------------------"
         puts "  Connecting concurrently to hosts '#{@zones_by_hosts.keys.join(' ')}' to perform migration (ZFS)."
-        Parallel.each(@zones_by_hosts.keys, in_processes: @zones_by_hosts.keys.size) do |host_name|
-          Parallel.each(@zones_by_hosts[host_name], in_processes: @zones_by_hosts[host_name].size) do |zone|
+        result_all = Parallel.map(@zones_by_hosts.keys, in_threads: @zones_by_hosts.keys.size) do |host_name|
+          Parallel.map(@zones_by_hosts[host_name], in_threads: @zones_by_hosts[host_name].size) do |zone|
+            log_name = "#{zone}_migration_#{rand(36**6).to_s(36)}.log"
+            routine_options[:logger] = Logger.new(File.join(log_dir, log_name))
+            puts "  Processing migration of zone '#{zone}:#{host_name}' to host #{@dest_host_spec[:host_name]}. See log '#{File.join(log_dir, log_name)}'."
             if host_name == 'localhost'
               SZMGMT::SZONES::SZONESMigrationRoutines.migrate_local_zone_zfs_archives(zone, @dest_host_spec)
             else
               SZMGMT::SZONES::SZONESMigrationRoutines.migrate_remote_zone_zfs_archives(zone, @host_specs[host_name], @dest_host_spec)
             end
+          end
+        end
+        puts "  ---------------------------------------------------------"
+        puts "  Migration finished."
+        puts "    Status:"
+        @zones_by_hosts.keys.each_with_index do |host_name, host_index|
+          puts "      #{host_name}:"
+          @zones_by_hosts[host_name].each_with_index do |zone_name, zone_index|
+            puts "        #{zone_name}: #{result_all[host_index][zone_index] ? 'success': 'failed'}"
+            CLI.zone_tracker.untrack_zone "#{zone_name}:#{host_name}" if result_all[host_index][zone_index]
+            CLI.zone_tracker.track_zone "#{zone_name}:#{@dest_host_spec[:host_name]}" if result_all[host_index][zone_index]
           end
         end
       end
@@ -93,13 +107,27 @@ module SZMGMT
         puts "    Rewrite existing zones: #{force ? "enable" : "disable"}"
         puts "  ---------------------------------------------------------"
         puts "  Connecting concurrently to hosts '#{@zones_by_hosts.keys.join(' ')}' to perform migration (UAR)."
-        Parallel.each(@zones_by_hosts.keys, in_processes: @zones_by_hosts.keys.size) do |host_name|
-          Parallel.each(@zones_by_hosts[host_name], in_processes: @zones_by_hosts[host_name].size) do |zone|
+        result_all = Parallel.map(@zones_by_hosts.keys, in_threads: @zones_by_hosts.keys.size) do |host_name|
+          Parallel.map(@zones_by_hosts[host_name], in_threads: @zones_by_hosts[host_name].size) do |zone|
+            log_name = "#{zone}_migration_#{rand(36**6).to_s(36)}.log"
+            routine_options[:logger] = Logger.new(File.join(log_dir, log_name))
+            puts "  Processing migration of zone '#{zone}:#{host_name}' to host #{@dest_host_spec[:host_name]}. See log '#{File.join(log_dir, log_name)}'."
             if host_name == 'localhost'
               SZMGMT::SZONES::SZONESMigrationRoutines.migrate_local_zone_uar(zone, @dest_host_spec)
             else
               SZMGMT::SZONES::SZONESMigrationRoutines.migrate_remote_zone_uar(zone, @host_specs[host_name], @dest_host_spec)
             end
+          end
+        end
+        puts "  ---------------------------------------------------------"
+        puts "  Migration finished."
+        puts "    Status:"
+        @zones_by_hosts.keys.each_with_index do |host_name, host_index|
+          puts "      #{host_name}:"
+          @zones_by_hosts[host_name].each_with_index do |zone_name, zone_index|
+            puts "        #{zone_name}: #{result_all[host_index][zone_index] ? 'success': 'failed'}"
+            CLI.zone_tracker.untrack_zone "#{zone_name}:#{host_name}" if result_all[host_index][zone_index]
+            CLI.zone_tracker.track_zone "#{zone_name}:#{@dest_host_spec[:host_name]}" if result_all[host_index][zone_index]
           end
         end
       end
